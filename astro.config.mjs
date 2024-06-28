c8fb2e7f1de4afbf7b24648e3b37eb2c5b69d2c2
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-28 16:05:07 -0400

    chore: hide Guides section in sidebar for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,13 +11,10 @@ export default defineConfig({
         github: 'https://github.com/withastro/starlight',
       },
       sidebar: [
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
-        },
+        // {
+        //   label: 'Guides',
+        //   items: [],
+        // },
         {
           label: 'Reference',
           autogenerate: { directory: 'reference' },
