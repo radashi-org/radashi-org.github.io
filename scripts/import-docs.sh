24f48420232035aa6aaf87ab3efc35c468834fb8
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-05 22:37:20 -0400

    chore: support multiple render scripts

--- scripts/import-docs.sh
+++ scripts/import-docs.sh
@@ -8,7 +8,14 @@ fi
 git clone https://github.com/radashi-org/radashi --depth 1
 mkdir -p src/content/docs/reference/
 
-sucrase-node scripts/generate-reference-index.ts
+# Find all scripts in ./scripts/ that start with "render-"
+render_scripts=$(find ./scripts -name "render-*.ts")
+
+# Run each script
+for script in $render_scripts; do
+  echo "Running $script"
+  sucrase-node "$script"
+done
 
 cd radashi/docs
 cp -r * ../../src/content/docs/reference
