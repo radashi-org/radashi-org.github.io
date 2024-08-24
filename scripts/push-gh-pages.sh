034fa1af8291ad2385511fafdc6846ce97b215a3
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-24 17:20:34 -0400

    chore: remove push-gh-pages script
    All publishing must be done through the "publish-docs" GitHub action in the radashi-org/radashi repository.

--- scripts/push-gh-pages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cd dist
-git init
-git remote add origin https://github.com/radashi-org/radashi-org.github.io
-git add -A
-git commit -m "chore: update docs"
-git push origin $(git rev-parse --abbrev-ref HEAD):gh-pages -f
