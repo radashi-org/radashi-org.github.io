d80ec17fa88024a44e4f34e7199744ee2b4f4a85
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-24 15:37:29 -0400

    chore: inline the public supabase creds

--- src/db/client.ts
+++ src/db/client.ts
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // Initialize Supabase client
 export const supabase = createClient(
-  import.meta.env.PUBLIC_SUPABASE_URL,
-  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+  'https://yucyhkpmrdbucitpovyj.supabase.co',
+  // This is an unauthenticated key, so it's safe to expose.
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1Y3loa3BtcmRidWNpdHBvdnlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAyMDQyNDcsImV4cCI6MjAzNTc4MDI0N30.t5isXFR9-AgiZUQ5g45a7O2pXlds4S8Y7LulYi7bOio'
 )
